6521336faa1f7cdd8e4b209427f0ae423d0ce2cd
vamshu <138673925+vamshu6164@users.noreply.github.com>
2023-07-05 17:12:34 +0530

    Update App.js
    removed routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+// import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -65,16 +65,18 @@ function App() {
   }
   return (
     <>
-    <Router>
+    // <Router>
         <Navbar title = "Textutils" mode={mode} radioActiveFun = {handleOnChangeId} toggleMode ={toggleModeFun} home="HOme" about="AbOut Us"/>
         <Alert alert={alert}/>
         <div className="container my-3">
-          <Routes>
-            <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Enter the Text to analyze below" mode={mode} radioActive = {radioActive}/>} />
-            <Route exact path='/about' element={<About/>} />
-          </Routes>
+          // <Routes>
+            // <Route exact path='/' element={
+    <TextForm showAlert={showAlert} heading="Enter the Text to analyze below" mode={mode} radioActive = {radioActive}/>
+    // } />
+            // <Route exact path='/about' element={<About/>} />
+          // </Routes>
         </div>
-    </Router>
+    // </Router>
     </>
   );
 }
